d7824a96685f0131859f19b59be6ff85415a0b25
Sean Khan <aliseankhan@gmail.com>
2026-06-04 18:42:08 +0100

    Set up DESCRIPTION, NAMESPACE and .Rbuildignore

---
+++ R/RforEvolution-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' @importFrom dplyr %>% .data
+#' @importFrom rlang :=
+#' @importFrom stats aggregate as.formula binomial coef complete.cases cor cov gaussian glm lm median na.omit poisson predict sd var
+#' @importFrom utils combn
+#' @importFrom grDevices colorRampPalette heat.colors
+NULL
