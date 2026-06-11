40983db272223f9da065cb9086c85e2fb2f43529
Sean Khan <aliseankhan@gmail.com>
2026-06-11 18:23:14 +0100

    Bundle the Bumpus sparrow data

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' Bumpus house sparrow survival data
+#'
+#' Morphological measurements and overwinter survival of house sparrows
+#' (\emph{Passer domesticus}) collected by Hermon Bumpus in 1898 after a severe
+#' winter storm in Providence, Rhode Island. It is a classic dataset for
+#' demonstrating natural selection on quantitative traits.
+#'
+#' @format A data frame with 136 rows and 11 variables:
+#' \describe{
+#'   \item{sex}{Sex of the bird (\code{male} or \code{female}).}
+#'   \item{survival}{Survival indicator: \code{1} if the bird survived, \code{0} otherwise.}
+#'   \item{total_length}{Total length (mm).}
+#'   \item{wingspread}{Alar extent / wingspread (mm).}
+#'   \item{weight}{Body weight (g).}
+#'   \item{head_length}{Length of head and beak (mm).}
+#'   \item{humerus}{Length of humerus (in).}
+#'   \item{femur}{Length of femur (in).}
+#'   \item{tibiotarsus}{Length of tibiotarsus (in).}
+#'   \item{skull_width}{Width of skull (in).}
+#'   \item{sternum}{Length of sternum / keel (in).}
+#' }
+#' @source Bumpus, H. C. (1899). The elimination of the unfit as illustrated by
+#'   the introduced sparrow, \emph{Passer domesticus}. \emph{Biological Lectures,
+#'   Marine Biology Laboratory, Woods Hole}, 209-226.
+"bumpus"
