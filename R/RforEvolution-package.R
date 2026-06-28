f9ac6024eb02bced2fefc618e2f7a6f703b20def
Sean Khan <aliseankhan@gmail.com>
2026-06-28 20:31:49 +0100

    Drop the rlang import

--- R/RforEvolution-package.R
+++ R/RforEvolution-package.R
@@ -2,7 +2,6 @@
 "_PACKAGE"
 
 #' @importFrom dplyr %>% .data
-#' @importFrom rlang :=
 #' @importFrom stats aggregate as.formula binomial coef complete.cases cor cov gaussian glm lm median na.omit poisson predict sd var
 #' @importFrom utils combn
 #' @importFrom grDevices colorRampPalette heat.colors
